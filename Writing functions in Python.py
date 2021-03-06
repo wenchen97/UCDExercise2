aefb396c8ff40e9a4bafe4cf7f5e330c477f4f7f
wenchen97 <wen.chen97@gmail.com>
2021-03-06 17:29:29 +0000

    6 Best practice

---
+++ Writing functions in Python.py
@@ -0,0 +1,114 @@
+def count_letter(content, letter):
+  """Count the number of times `letter` appears in `content`.
+
+  # Add a Google style arguments section
+  Args:
+   content (str): The string to search.
+   letter (str): The letter to search for.
+   Returns:
+    int
+
+  # Add a section detailing what errors might be raised
+  Raises:
+    ValueError: If `letter` is not a one-character string.
+  """
+  """
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('`letter` must be a single character string.')
+  return len([char for char in content if char == letter])
+  
+  
+import inspect
+
+# Get the docstring with a function from the inspect module
+docstring = inspect.getdoc(count_letter)
+
+border = '#' * 28
+print('{}\n{}\n{}'.format(border, docstring, border))
+  
+def build_tooltip(function):
+  """
+  Create
+  a
+  tooltip
+  for any function that shows the
+  function
+  's docstring.
+
+  Args:
+  function(callable): The
+  function
+  we
+  want
+  a
+  tooltip
+  for .
+
+
+Returns:
+str
+"""
+# Use 'inspect' to get the docstring
+docstring = inspect.getdoc(function)
+border = '#' * 28
+return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
+
+def standardize(column):
+  """Standardize the values in a column.
+
+  Args:
+    column (pandas Series): The data to standardize.
+
+  Returns:
+    pandas Series: the values as z-scores
+  """
+  # Finish the function so that it returns the z-scores
+  z_score = (column - column.mean()) / column.std()
+  return z_score
+
+# Use the standardize() function to calculate the z-scores
+df['y1_z'] = standardize(df.y1_gpa)
+df['y2_z'] = standardize(df.y2_gpa)
+df['y3_z'] = standardize(df.y3_gpa)
+df['y4_z'] = standardize(df.y4_gpa)
+  
+def median(values):
+  """Get the median of a list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the median() function
+  midpoint = int(len(values)/2)
+  if len(values)%2==0:
+    median = (values[midpoint - 1] + values[midpoint]) / 2
+  else:
+     median = values[midpoint]
+  return median
+  
+# Use an immutable variable for the default argument 
+def better_add_column(values, df=None):
+  """Add a column of `values` to a DataFrame `df`.
+  The column will be named "col_<n>" where "n" is
+  the numerical index of the column.
+
+  Args:
+    values (iterable): The values of the new column
+    df (DataFrame, optional): The DataFrame to update.
+      If no DataFrame is passed, one is created by default.
+
+  Returns:
+    DataFrame
+  """
+  # Update the function to create a default DataFrame
+  if df is None:
+    df = pandas.DataFrame()
+  df['col_{}'.format(len(df.columns))] = values
+  return df
